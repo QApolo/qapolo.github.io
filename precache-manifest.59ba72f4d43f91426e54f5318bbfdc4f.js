a3677561157391a96eeb51bb0465cfc8ff3b3f3b
Juan Carlos Garcia Medina <carlos_isc7@outlook.com>
2020-07-03 19:34:36 -0500

    Updates

---
+++ precache-manifest.59ba72f4d43f91426e54f5318bbfdc4f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3293af0f17409e495624584b5cb4e5d",
+    "url": "/05_CovidProj/index.html"
+  },
+  {
+    "revision": "2719f8700a7ca529f769",
+    "url": "/05_CovidProj/static/css/main.93bcfeea.chunk.css"
+  },
+  {
+    "revision": "3633f0920a5108a351be",
+    "url": "/05_CovidProj/static/js/2.e8beddea.chunk.js"
+  },
+  {
+    "revision": "28bcb252a64156eb2fda27866b95e924",
+    "url": "/05_CovidProj/static/js/2.e8beddea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2719f8700a7ca529f769",
+    "url": "/05_CovidProj/static/js/main.f782f7d0.chunk.js"
+  },
+  {
+    "revision": "56b96ace61fbbc172f7b",
+    "url": "/05_CovidProj/static/js/runtime-main.66e16144.js"
+  },
+  {
+    "revision": "70987b9a7719c12de1c82bbbffbf7b77",
+    "url": "/05_CovidProj/static/media/pause.70987b9a.png"
+  }
+]);
